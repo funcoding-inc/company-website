5bc9d9ea4d1c86a6e71175e14f9cf8ee76e9fe61
Tetsuyuki Okuzono <fe.snow1@gmail.com>
2023-11-28 12:23:27 +0700

    Remove unused arguments

--- src/utils/bin/utils.ts
+++ src/utils/bin/utils.ts
@@ -1,7 +1,7 @@
 import packageJson from '../../../package.json';
 import * as bin from './index';
 
-export const help = async (args: string[]): Promise<string> => {
+export const help = async (): Promise<string> => {
   const commands = Object.keys(bin).sort().join(', ');
 
   return `Available commands:\n${commands}\n\n[tab]\t trigger completion.\n[ctrl+l] clear terminal.\n[ctrl+c] cancel command.`;
@@ -11,35 +11,35 @@ export const echo = async (args: string[]): Promise<string> => {
   return args.join(' ');
 };
 
-export const whoami = async (args: string[]): Promise<string> => {
+export const whoami = async (): Promise<string> => {
   return 'guest';
 };
 
-export const date = async (args: string[]): Promise<string> => {
+export const date = async (): Promise<string> => {
   return new Date().toString();
 };
 
-export const gui = async (args: string[]): Promise<string> => {
+export const gui = async (): Promise<string> => {
   window.open('https://m4tt72.com', '_self');
 
   return 'Opening GUI version...';
 };
 
-export const contact = async (args: string[]): Promise<string> => {
+export const contact = async (): Promise<string> => {
   window.open('mailto:support@funcoding.jp');
 
   return 'Opening mailto:support@funcoding.jp...';
 };
 
-export const vi = async (args: string[]): Promise<string> => {
+export const vi = async (): Promise<string> => {
   return `why use vi? try 'emacs'.`;
 };
 
-export const vim = async (args: string[]): Promise<string> => {
+export const vim = async (): Promise<string> => {
   return `why use vim? try 'emacs'.`;
 };
 
-export const emacs = async (args?: string[]): Promise<string> => {
+export const emacs = async (): Promise<string> => {
   return `really? emacs? you should be using 'vim'`;
 };
 
@@ -51,7 +51,7 @@ export const sudo = async (args?: string[]): Promise<string> => {
   return `Permission denied: unable to run the command '${args[0]}' as root.`;
 };
 
-export const repo = async (args?: string[]): Promise<string> => {
+export const repo = async (): Promise<string> => {
   setTimeout(function () {
     window.open('https://github.com/m4tt72/terminal', '_blank');
   }, 1000);
@@ -59,13 +59,13 @@ export const repo = async (args?: string[]): Promise<string> => {
   return 'Opening repository...';
 };
 
-export const donate = async (args?: string[]): Promise<string> => {
+export const donate = async (): Promise<string> => {
   window.open(packageJson.funding.url, '_blank');
 
   return 'Opening donation url...';
 };
 
-export const info = async (args: string[]): Promise<string> => {
+export const info = async (): Promise<string> => {
   return `
 Funcoding Inc.
 
@@ -80,7 +80,7 @@ Minamitokiwadai 1-11-6-101, Itabashi-ku, Tokyo, Japan
 `;
 };
 
-export const banner = (args?: string[]): string => {
+export const banner = (): string => {
   return `
 ███████╗██╗   ██╗███╗   ██╗ ██████╗ ██████╗ ██████╗ ██╗███╗   ██╗ ██████╗
 ██╔════╝██║   ██║████╗  ██║██╔════╝██╔═══██╗██╔══██╗██║████╗  ██║██╔════╝
