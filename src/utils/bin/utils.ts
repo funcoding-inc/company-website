e783ed7ed44b70bb07120c74f0839386928bdb70
Tetsuyuki Okuzono <fe.snow1@gmail.com>
2023-11-28 17:44:25 +0900

    Remove info command

--- src/utils/bin/utils.ts
+++ src/utils/bin/utils.ts
@@ -33,21 +33,6 @@ export const repo = async (): Promise<string> => {
   return 'Opening repository...';
 };
 
-export const info = async (): Promise<string> => {
-  return `
-Funcoding Inc.
-
-Established 2014-04-01
-
-CEO Tetsuyuki Okuzono
-
-〒174-0072
-Minamitokiwadai 1-11-6-101, Itabashi-ku, Tokyo, Japan
-
-3,000,000 yen capital
-`;
-};
-
 export const banner = (): string => {
   return `
 ███████╗██╗   ██╗███╗   ██╗ ██████╗ ██████╗ ██████╗ ██╗███╗   ██╗ ██████╗
